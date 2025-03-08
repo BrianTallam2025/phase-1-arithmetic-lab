83f639c235774b1fd4f56a6b0219aea68afbc6ee
Brian Tallam <tallambrian633@gmail.com>
2025-03-08 16:04:50 +0300

    basic functions

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 = 2;
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiply(num1, num2));
 
-// Write your code here
+const random = Math.floor(Math.random() * 1000) + 1;
+console.log(random);
+
+const num3 = 10;
+const num4 = 6;
+function mod(num3,num4) {
+    return num3 % num4;
+}
+console.log(mod(num3, num4));
+
+const max = Math.max(1, 2, 20);
+console.log(max);
